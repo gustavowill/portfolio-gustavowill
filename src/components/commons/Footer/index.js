36232babd0b8b070a6b32a0420a09cfa4dafb6c2
Gustavo Willemann <gustavowill@hotmail.com>
2021-02-21 00:50:40 -0300

    Corrigido link para o github.

--- src/components/commons/Footer/index.js
+++ src/components/commons/Footer/index.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <FooterWrapper>
       <FooterWrapper.LinksBar>
-        <FooterWrapper.Icon href="https://github.com/gustavowill"><img src="/images/icon/github.svg" alt="github icon" /></FooterWrapper.Icon>
+        <FooterWrapper.Icon href="https://github.com/guswillemann"><img src="/images/icon/github.svg" alt="github icon" /></FooterWrapper.Icon>
         <FooterWrapper.Icon href="https://twitter.com/guswillemann"><img src="/images/icon/twitter.svg" alt="twitter icon" /></FooterWrapper.Icon>
       </FooterWrapper.LinksBar>
     </FooterWrapper>
--- src/components/commons/Footer/styles.js
+++ src/components/commons/Footer/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import theme from '../../../theme';
 
 const FooterWrapper = styled.section`
+  vertical-align: bottom;
   display: flex;
   justify-content: center;
   align-items: center;
